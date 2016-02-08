fb93208bcecc9bdff63863af05bb08762c1931fe
Olivier Beddows <olivier@lisk.io>
2016-02-08 09:29:02 +0000

    Changing address format.

--- helpers/account.js
+++ helpers/account.js
@@ -3,7 +3,7 @@ var crypto = require('../lib/crypto.js');
 module.exports = {
 	account: function (secret) {
 		var kp = crypto.keypair(secret);
-		var address = crypto.getId(new Buffer(kp.publicKey, 'hex')) + "C";
+		var address = crypto.getId(new Buffer(kp.publicKey, 'hex')) + 'L';
 
 		return {
 			keypair: kp,
